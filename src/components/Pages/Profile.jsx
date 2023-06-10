a5d2fedebc805a60975a8336b12a20bbfce03ca9
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-06-10 12:45:17 +0200

    added quick test for profile

--- src/components/Pages/Profile.jsx
+++ src/components/Pages/Profile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import { useParams } from "react-router-dom"
+import { GameContext } from "../../context/games";
+import { useContext } from "react";
+import useApiFetcher from "../../hooks/useApiFetcher";
 const Profile = () => {
+  //getting userName... maybe later use id
+  const {name} = useParams();
+  const {data, isLoading, error} = useApiFetcher("/users?username="+name);
+  //getting all games
+  const {data: games, isLoading: gamesLoading, error: gamesError} = useContext(GameContext);
+  
   return (
-    <div className='mainContent'>Profile</div>
-  )
+    <div className='mainContent'>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {data[0]?.username && <div>
+        <h1>{data[0].username}</h1>
+        <h2>Games:</h2>
+        {gamesLoading && <div>Loading...</div>}
+        {gamesError && <div>{gamesError}</div>}
+        {games && <div>
+          {games.map((game) => {
+            if(game.uid === data[0].uid){
+              return <div key={game.name}>{game.name}</div>
+            }
+            return null})}</div>}</div>}
+      </div>
+    )
 }
 
 export default Profile
