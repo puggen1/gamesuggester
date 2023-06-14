0e6aa9aa1a5cc9f29a22fa2478ac2521f336e76b
puggen1 <43915072+puggen1@users.noreply.github.com>
2023-06-14 09:02:06 +0200

    Revert "removed logs"
    This reverts commit 6c0adbd3b09126bea928cc0e71838368808a0807.

--- src/hooks/useApiFetcher.js
+++ src/hooks/useApiFetcher.js
@@ -19,6 +19,8 @@ const useApiFetcher = (subUrl) => {
   const fetcher = async () => {
     try {
       setIsLoading(true);
+      console.log(fullUrl, options)
+
       const response = await fetch(fullUrl, options);
       const result = await response.json();
       setData(result);
