df85e2e48040e853552aa35eca0daba62326aa9f
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-07-31 11:43:18 +0200

    some code for add game
    started adding things, there is some problems with duplicates in the steamGameResponse...

--- src/components/Pages/AddGame.jsx
+++ src/components/Pages/AddGame.jsx
@@ -1,22 +1,59 @@
-import { Autocomplete, TextField } from "@mui/material"
+import { Autocomplete, TextField,Button, Typography } from "@mui/material"
+import { OuterAddGame } from "../Game/index.styles"
 import useApiFetcher from "../../hooks/useApiFetcher"
+import { useEffect, useState } from "react"
 const AddGame = () => {
     const token = localStorage.getItem("token")
     const {data, isLoading, error} = useApiFetcher("steamgames", token) 
- 
-  return (
-    <div>
+    const [chosenGame, setChosenGame] = useState(null)
+    //removes duplicates
+    const [noDuplicates, setNoDuplicates] = useState(new Set(data))
+    //have to make a new array, because the set is not iterable
+    const allGames = [...noDuplicates]
+    useEffect(() => {
+        if(data){
+            const noDuplicates = new Set(data)
+            setNoDuplicates(noDuplicates)
+        }
+    }, [data])
+    return (
+    <OuterAddGame>
         {isLoading && <p>loading</p>}
         {error && <p>error</p>}
-        {Object.keys(data).length > 0 &&
-        <Autocomplete options={data}
+        {noDuplicates.size > 0 &&
+        <>
+        <Typography variant="h4" component="h1" color="white" textAlign="center">Add a game</Typography>
+        <div className="search">
+          <Typography variant="h5" component="h2" color="white">1. Search for a game</Typography>
+          <Autocomplete options={allGames}
          disablePortal
          getOptionLabel={(option)=>{return option.name}}
-         renderOption={(props, option)=>{return <li {...props} key={option.appId}>{option.name} </li>}}
+         renderOption={(props, option)=>{   return <li {...props} key={option.appID}>{option.name} </li>}}
          renderInput={(params) => <TextField {...params} label="Games" />}
-           />}
-    </div>
+           />
+        </div>
+        {chosenGame && <div className="validate">
+          <Typography variant="h5" component="h2" color="white">2. Look trough the game</Typography>
+        </div>}
+{chosenGame && <div className="confirm">
+      <Typography variant="h5" component="h2" color="white">3. Confirm</Typography>
+          <Button>Cancel</Button>
+          <Button>Add game</Button>
+        </div>}
+          </>
+        }
+    </OuterAddGame>
   )
 }
 
 export default AddGame
+/**
+ * <Autocomplete options={data}
+         disablePortal
+         getOptionLabel={(option)=>{return option.name}}
+         renderOption={(props, option)=>{return <li {...props} key={option.appId}>{option.name} </li>}}
+         renderInput={(params) => <TextField {...params} label="Games" />}
+           />
+ */
+
+          
