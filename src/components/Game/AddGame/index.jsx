e7ecad51e97bfc0380c99e10981fc7af1d283c4f
bendik kvam <bendik.kvam61@gmail.com>
2024-10-08 10:19:19 +0200

    added snackbar around alert

--- src/components/Game/AddGame/index.jsx
+++ src/components/Game/AddGame/index.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react"
-import {Alert, AlertTitle, Button, Typography, useTheme } from "@mui/material"
+import React, { useState, useEffect } from "react"
+import {Snackbar,Alert,AlertTitle, Button, Typography, useTheme } from "@mui/material"
+
 import useApiFetcher from "../../../hooks/useApiFetcher"
 import useSendData from "../../../hooks/useSendData"
 import SteamGameSearch from "../../SteamGameSearch"
 import AddGameCard from "../AddGameCard"
 import { Clear, Check, Close} from "@mui/icons-material"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import ConfirmGameSkeleton from "../../PlaceholderSkeleton/ConfirmGameSkeleton"
 import AlreadyAdded from "../AlreadyAdded"
 /**
@@ -14,6 +15,7 @@ import AlreadyAdded from "../AlreadyAdded"
  * @description  
  */
 const AddGame = ({data}) => {
+    const navigate = useNavigate()
     const theme = useTheme()
     const token = localStorage.getItem("token")
     const [chosenGame, setChosenGame] = useState(null)
@@ -21,6 +23,7 @@ const AddGame = ({data}) => {
     const [duplicateUrl, setDuplicateUrl] = useState("");
     const [noDuplicates, setNoDuplicates] = useState()
     const [success, setSuccess] = useState(false);
+    const [isOpen, setIsOpen] = useState(false)
     const {data: chosenGameData, isLoading: chosenGameLoadingData, error: chosenGameErrorData} = useApiFetcher(singleGameUrl, token)
     const {data: isDuplicateData, isLoading: isDuplicateDataLoading, error: isDuplicateDataError} = useApiFetcher(duplicateUrl)
     const {sender} = useSendData()
@@ -52,6 +55,7 @@ const AddGame = ({data}) => {
     const cancel = ()=>{
       setChosenGame(null)
       setSuccess(false)
+      setIsOpen(false)
     }
 
     /**
@@ -61,11 +65,13 @@ const AddGame = ({data}) => {
       const response = await sender("games/add", "POST", {id:chosenGameData.appID}, token)
       if(response.success){
         setSuccess(true)
+        setIsOpen(true)
       }
       else{
         setSuccess(false)
       }
     }
+   
   return (
     (noDuplicates && noDuplicates.length > 0) &&
         <>
@@ -81,20 +87,25 @@ const AddGame = ({data}) => {
           {chosenGameErrorData && <p>test error</p>}
           {(!chosenGameLoadingData && !isDuplicateData.found) && <AddGameCard gameData={chosenGameData}/>}
         </div>}
-{((chosenGame && !chosenGameLoadingData) && (!isDuplicateData.found && !isDuplicateDataLoading)) && <div className="confirm">
+{((chosenGame && !chosenGameLoadingData) && (!isDuplicateData.found && !isDuplicateDataLoading)) &&<> <div className="confirm">
       <Typography variant="h5" component="h2" color="white">3. Confirm</Typography>
       <div className="actionButtons">
           <Button sx={{backgroundColor:theme.palette.secondary.main, width:"150px"}} startIcon={<Clear color="warning"/>} color="warning" onClick={cancel}>Cancel</Button>
           <Button sx={{backgroundColor:theme.palette.secondary.main, width:"150px"}} startIcon={<Check color="warning"/>} color="warning" onClick={confirm}>Add game</Button>
           </div>
-          {(success && chosenGame) &&
-          <Alert severity="success">
-            <AlertTitle>Game Added!</AlertTitle>
-            {chosenGame.name} has been added! <strong><Link to={"/game/" + chosenGame.appID}>view game</Link></strong>
-            <Button onClick={()=>{setSuccess(false)}}><Close /></Button>
-          </Alert>
-          }
-        </div>}
+        </div>
+         <Snackbar open={isOpen}
+         autoHideDuration={10000}
+         onClose={cancel}>
+            <Alert severity="success">
+           <AlertTitle>Game Added!</AlertTitle>
+           {chosenGame?.name ? chosenGame.name : ""} has been added! <strong><Link to={"/game/" + chosenGame.appID}>view game</Link></strong>
+           <Button onClick={()=>{cancel()}}><Close /></Button>
+         </Alert>
+         </Snackbar>
+        </>
+        }
+       
           </>
   )
 }
